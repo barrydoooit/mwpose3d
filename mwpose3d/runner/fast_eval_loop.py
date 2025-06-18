124674796a4020a73bd37b55964f1cc9ed1af37f
Lucan de Groot <L.J.deGroot@tudelft.nl>
2025-06-18 11:30:42 +0200

    Add fast eval option

---
+++ mwpose3d/runner/fast_eval_loop.py
@@ -0,0 +1,50 @@
+from typing import TYPE_CHECKING, Dict, Union
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from mwpose3d.evaluation.metrics.base import BaseMetric
+from mwpose3d.registry import METRICS, LOOPS
+from .base_loop import BaseLoop
+
+if TYPE_CHECKING:
+    from mwpose3d.runner.runner import Runner
+
+
+@LOOPS.register_module()
+class FastEvalLoop(BaseLoop):
+    def __init__(
+            self,
+            runner: 'Runner',
+            dataloader: Union[DataLoader, Dict]
+    ):
+        super().__init__(runner, dataloader)
+        self._reset()
+
+    def _reset(self):
+        self._loss = 0
+        self._sum_loss = 0
+        self._loss_count = 0
+
+    def run(self) -> float:
+        """
+        Evaluate the model and return the loss
+        """
+        self._reset()
+        self._run_epoch()
+        return self._loss
+
+    def _run_epoch(self) -> None:
+        self.runner.model.eval()
+        with torch.no_grad():
+            for idx, data_batch in tqdm(enumerate(self.dataloader),
+                          total=len(self.dataloader),
+                          desc='Validating'):
+                self._run_iter(idx, data_batch)
+
+    def _run_iter(self, idx: int, data_batch: dict) -> None:
+        batch_inputs, data_samples = self.runner.model.pack_input(data_batch)
+        loss = self.runner.model(batch_inputs, data_samples, mode='loss')
+        self._sum_loss += loss.item()
+        self._loss_count += 1
+        self._loss = self._sum_loss / self._loss_count
--- mwpose3d/runner/train_loop.py
+++ mwpose3d/runner/train_loop.py
@@ -1,9 +1,11 @@
-from typing import TYPE_CHECKING, Dict, Sequence, Union
+from typing import TYPE_CHECKING, Dict, Sequence, Union, Optional
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from .base_loop import BaseLoop
 from mwpose3d.registry import LOOPS
+from pathlib import Path
+from mwpose3d.runner.fast_eval_loop import FastEvalLoop
 
 if TYPE_CHECKING:
     from mwpose3d.runner.runner import Runner
@@ -16,7 +18,8 @@ class EpochBasedTrainLoop(BaseLoop):
         dataloader: Union[DataLoader, Dict],
         max_epochs: int,
         val_begin: int = 1,
-        val_interval: int = 1
+        val_interval: int = 1,
+        out_file: Optional[str] = None
     ):
         super().__init__(runner, dataloader)
         self._max_epochs = int(max_epochs)
@@ -24,10 +27,23 @@ class EpochBasedTrainLoop(BaseLoop):
             f'`max_epochs` should be a integer number, but get {max_epochs}.'
         self._max_iters = self._max_epochs * len(self.dataloader)
         self._epoch = 0
+        self._epoch_loss = 0
+        self._epoch_sum_loss = 0
+        self._epoch_loss_count = 0
         self._iter = 0
         self.val_begin = val_begin
         self.val_interval = val_interval
         self.stop_training = False
+        self.out_file = out_file
+        if self.out_file is not None:
+            self.out_file = Path(self.runner.work_dir / self.out_file)
+            if not self.out_file.parent.exists():
+                self.out_file.parent.mkdir(parents=True, exist_ok=True)
+            else:
+                with open(self.out_file, 'w') as f:
+                    f.write('epoch, train_loss, validation_loss\n')
+
+
         
     @property
     def max_epochs(self):
@@ -49,6 +65,10 @@ class EpochBasedTrainLoop(BaseLoop):
         """int: Current iteration."""
         return self._iter
 
+    def _update_out_file(self, train_loss, validation_loss):
+        with open(self.out_file, 'a') as f:
+            f.write(f'{self.epoch}, {train_loss}, {validation_loss}\n')
+
     def run(self) -> torch.nn.Module:
         self.runner.call_hook('before_train')
         
@@ -69,13 +89,17 @@ class EpochBasedTrainLoop(BaseLoop):
                     and self._epoch >= self.val_begin
                     and (self._epoch % self.val_interval == 0
                          or self._epoch == self._max_epochs)):
-                self.runner.val_loop.run()
+                ret = self.runner.val_loop.run()
+                if isinstance(self.runner.val_loop, FastEvalLoop):
+                    if self.out_file is not None:
+                        self._update_out_file(self._epoch_loss, ret)
                 self.runner.save_checkpoint(f'epoch_{self._epoch}.pth')
         
         self.epoch_pbar.close()
         self.runner.call_hook('after_train')
         return self.runner.model
-    
+
+
     def _run_epoch(self) -> None:
         self.runner.call_hook('before_train_epoch')
         self.runner.model.train()
@@ -93,14 +117,17 @@ class EpochBasedTrainLoop(BaseLoop):
         self.runner.optimizer.zero_grad()
         loss.backward()
         self.runner.optimizer.step()
+        self._epoch_sum_loss += loss.item()
+        self._epoch_loss_count += 1
+        self._epoch_loss = self._epoch_sum_loss / self._epoch_loss_count
         if self._iter % 30 == 0:
-            self.epoch_pbar.set_postfix_str(f'loss: {loss.item():.4f}')
+            self.epoch_pbar.set_postfix_str(f'loss: {self._epoch_loss:.4f}')
         
         self.runner.call_hook(
             'after_train_iter',
             batch_idx=idx,
             data_batch=data_batch,
-            outputs=loss)
+            outputs=self._epoch_loss)
         self._iter += 1
         
         
